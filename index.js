93f54732470db8b78905e86a37a5a7e8f59b72ec
erodxd <emrod718@gmail.com>
2019-04-22 13:13:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+
+function heyJude(str) {
+    return (str.repeat(16))
+}
+heyJude
+
+function prohibitedLanguage(str) {
+    return (str.search(/candycorn|brusselssprouts/))
+}
+prohibitedLanguage
