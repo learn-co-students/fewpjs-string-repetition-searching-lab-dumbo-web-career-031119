da1b631d692600a843fe0015497b9803d49cb4e2
Jeff Yu <jwy5140@gmail.com>
2019-04-22 14:01:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function heyJude() {
+    return "Na na na na na na na, na na na na, hey Jude.".repeat(16);
+}
+
+function prohibitedLanguage(arg){
+    let myRegExp = /candycorn|brusselssprouts/;
+    let result = arg.search(myRegExp);
+    return result;
+}
