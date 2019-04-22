4f17369dcbf0c7016454c369883b60612494b5da
github username <itsDario1@gmail.com>
2019-04-22 13:23:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+
+function heyJude(verse) {
+    return verse.repeat(16);
+}
+
+
+
+function prohibitedLanguage(uname) {
+    return uname.search(/candycorn|brusselssprouts/);
+}
